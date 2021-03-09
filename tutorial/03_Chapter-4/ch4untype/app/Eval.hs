7474feb445ffcb66b063d23b7fbcfef42643cc31
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 11:30:36 -0600

    First section of the eval file to evaluate the values for integer, boolean, and closure

--- tutorial/03_Chapter-4/ch4untype/app/Eval.hs
+++ tutorial/03_Chapter-4/ch4untype/app/Eval.hs
@@ -1,1 +1,20 @@
 module Eval where
+
+import Syntax
+
+data Value
+  = VInt Integer
+  | VBool Bool
+  | VClosure String Expr (Eval.Scope)
+
+instance Show Value where
+  show (VInt x) = show x
+  show (VBool x) = show x
+  show VClosure {} = "<<closure>>"
+
+data EvalState =
+  EvalState
+    { depth :: Int
+    }
+  deriving (Show)
+
