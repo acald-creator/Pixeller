f499e91e06b52c32fb6b811bebb9755a7c8fe173
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 10:53:44 -0600

    Added the first section to parse lambda

--- tutorial/03_Chapter-4/ch4untype/app/Parser.hs
+++ tutorial/03_Chapter-4/ch4untype/app/Parser.hs
@@ -5,4 +5,14 @@ import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Language
 
+import qualified Text.Parsec.Token as Tok
+
 import Syntax
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+	where ops = ["->","\\","+","*","-","="]
+	names = []
+	style = haskellStyle {Tok.reservedOpNames = ops,
+                              Tok.reservedNames = names,
+                              Tok.commentLine = "#"}
