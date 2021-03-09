2112330745caed11723919ce1c48f2f55c13eec2
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 10:29:52 -0600

    Set up the syntax

--- tutorial/03_Chapter-4/ch4untype/app/Syntax.hs
+++ tutorial/03_Chapter-4/ch4untype/app/Syntax.hs
@@ -1,1 +1,17 @@
 module Syntax where
+
+-----------------------------------------------------
+type Name = String
+
+data Expr
+  = Var Name
+  | Lit Lit
+  | App Expr Expr
+  | Lam Name Expr
+  deriving (Eq, Show)
+
+data Lit
+  = LInt Int
+  | LBool Bool
+  deriving (Show, Eq, Ord)
+-----------------------------------------------------
