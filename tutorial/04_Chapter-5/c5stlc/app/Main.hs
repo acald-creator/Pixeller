67a312aea44fd18f2fded9f0a01819ad0613d9ed
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 16:31:02 -0600

    Finished main as the main entrypoint for repl

--- tutorial/04_Chapter-5/c5stlc/app/Main.hs
+++ tutorial/04_Chapter-5/c5stlc/app/Main.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import Check
+import Eval
+import Parser
+import Pretty
+import Syntax
+
+import Control.Monad.Trans
+import System.Console.Haskeline
+
+-----------------------------------------------------------
+process :: String -> IO ()
+process line = do
+  let res = parseExpr line
+  case res of
+    Left err -> print err
+    Right ex -> do
+      let chk = checkTop [] ex
+      case chk of
+        Left tyerr -> print tyerr
+        Right _ -> print $ runEval ex
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runInputT defaultSettings loop
+  where
+    loop = do
+      minput <- getInputLine "Happy> "
+      case minput of
+        Nothing -> outputStrLn "Goodbye."
+        Just input -> (liftIO $ process input) >> loop
+-----------------------------------------------------------
