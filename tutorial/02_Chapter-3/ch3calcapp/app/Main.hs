e5bfb78da3529f673399a53e3a4dc539f70346dc
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-08 21:05:26 -0600

    Set up repl on main

--- tutorial/02_Chapter-3/ch3calcapp/app/Main.hs
+++ tutorial/02_Chapter-3/ch3calcapp/app/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Eval
+import Parser
+
+process :: String -> IO ()
+process line = do
+  let res = parseExpr line
+  case res of
+    Left err -> print err
+    Right ex -> print $ runEval ex
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runInputT defaultSettings loop
+  where
+    loop = do
+      minput <- getInputLine "Repl> "
+      case minput of
+        Nothing -> outputStrLn "Goodbye."
+        Jut input -> (liftIO $ process input) >> loop
