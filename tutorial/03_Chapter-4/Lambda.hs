de84d32704bd486e220893d5a1b06c20ffbc8edf
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 10:30:19 -0600

    Created Lambda example

---
+++ tutorial/03_Chapter-4/Lambda.hs
@@ -0,0 +1,18 @@
+module Lambda where
+
+type Name = String
+
+data Expr
+  = Var Name
+  | App Expr Expr
+  | Lam Name Expr
+
+s, k, i :: Expr
+i = Lam "x" (Var "x")
+
+k = Lam "x" (Lam "y" (Var "x"))
+
+s =
+  Lam
+    "x"
+    (Lam "y" (Lam "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z")))))
