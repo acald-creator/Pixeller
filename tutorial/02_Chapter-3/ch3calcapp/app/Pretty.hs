9c730360b2ec9f3d57fa7c17150be7c928aed4fa
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 09:45:33 -0600

    Lint this file, to remove redundant brackets

--- tutorial/02_Chapter-3/ch3calcapp/app/Pretty.hs
+++ tutorial/02_Chapter-3/ch3calcapp/app/Pretty.hs
@@ -20,9 +20,9 @@ instance Pretty Expr where
   ppr _ Zero = PP.text "0"
   ppr _ Tr = PP.text "true"
   ppr _ Fl = PP.text "false"
-  ppr p (Succ a) = (parensIf (p > 0) $ PP.text "succ" <+> ppr (p + 1) a)
-  ppr p (Pred a) = (parensIf (p > 0) $ PP.text "succ" <+> ppr (p + 1) a)
-  ppr p (IsZero a) = (parensIf (p > 0) $ PP.text "iszero" <+> ppr (p + 1) a)
+  ppr p (Succ a) = parensIf (p > 0) $ PP.text "succ" <+> ppr (p + 1) a
+  ppr p (Pred a) = parensIf (p > 0) $ PP.text "succ" <+> ppr (p + 1) a
+  ppr p (IsZero a) = parensIf (p > 0) $ PP.text "iszero" <+> ppr (p + 1) a
   ppr p (If a b c) =
     PP.text "if" <+>
     ppr p a <+> PP.text "then" <+> ppr p b <+> PP.text "else" <+> ppr p c
