7f85e948674b80998fc8cece0d2ac7e9c262b048
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 12:58:59 -0600

    Changed up the main to process received inputs through repl

--- tutorial/03_Chapter-4/ch4untype/app/Main.hs
+++ tutorial/03_Chapter-4/ch4untype/app/Main.hs
@@ -1,4 +1,36 @@
 module Main where
 
+import Eval
+import Parser
+import Pretty
+import Syntax
+
+import Control.Monad
+import Control.Monad.Trans
+import System.Console.Haskelline
+
+---------------------------------------------------------------------------------
+showStep :: (Int, Expr) -> IO ()
+showStep (d, x) = putStrLn ((replicate d ' ') ++ "=> " ++ ppexpr x)
+
+---------------------------------------------------------------------------------
+process :: String -> IO ()
+process line = do
+  let res = parseExpr line
+  case res of
+    Left err -> print err
+    Right ex -> do
+      let (out, ~steps) = runEval ex
+      mapM_ showStep steps
+      print out
+
+---------------------------------------------------------------------------------
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runInputT defaultSettings loop
+  where
+    loop = do
+      minput <- getInputLine "Untyped> "
+      case minput of
+        Nothing -> outputStrLn "Goodbye."
+        Just input -> (liftIO $ process input) >> loop
+---------------------------------------------------------------------------------
