4244f1437aaf1df3a79b10526cd2f45726822706
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 14:12:28 -0600

    Fixed the import issues

--- tutorial/03_Chapter-4/ch4untype/app/Pretty.hs
+++ tutorial/03_Chapter-4/ch4untype/app/Pretty.hs
@@ -1,22 +1,41 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Pretty
   ( ppexpr
   ) where
 
-----------------------------------------------------------------
+-----------------------------------------------------------------------------------
 import Syntax
 
-import Text.PrettyPrint
+import Text.PrettyPrint (Doc, (<+>), char, hsep, parens, render, sep, text)
 
-----------------------------------------------------------------
+-----------------------------------------------------------------------------------
 class Pretty p where
   ppr :: Int -> p -> Doc
-  pp :: p -> Doc
-  pp = ppr 0
 
-----------------------------------------------------------------
+-----------------------------------------------------------------------------------
+--- Define ppr
+-----------------------------------------------------------------------------------
+instance Pretty Name where
+  ppr _ x = text x
+
+instance Pretty Expr where
+  ppr _ (Var x) = text x
+  ppr _ (Lit (LInt a)) = text (show a)
+  ppr _ (Lit (LBool b)) = text (show b)
+  ppr p e@(App _ _) = parensIf (p > 0) (ppr p f <+> sep (map (ppr (p + 1)) xs))
+    where
+      (f, xs) = viewApp e
+  ppr p e@(Lam _ _) =
+    parensIf (p > 0) $ (char '\\' <> hsep vars) <+> text "." <+> body
+    where
+      vars = map (ppr 0) (viewVars e)
+      body = ppr (p + 1) (viewBody e)
+
+-----------------------------------------------------------------------------------
 --- Create two helper functions that collapse lambda bindings
 --- so they can be printed out as single lambda expressions
-----------------------------------------------------------------
+-----------------------------------------------------------------------------------
 viewVars :: Expr -> [Name]
 viewVars (Lam n a) = n : viewVars a
 viewVars _ = []
@@ -32,28 +51,14 @@ viewApp (App e1 e2) = go e1 [e2]
     go f xs = (f, xs)
 viewApp _ = error "not application"
 
-----------------------------------------------------------------
+-----------------------------------------------------------------------------------
 --- Create a helper function for parenthesizing subexpressions
-----------------------------------------------------------------
+-----------------------------------------------------------------------------------
 parensIf :: Bool -> Doc -> Doc
 parensIf True = parens
 parensIf False = id
 
-----------------------------------------------------------------
---- Define ppr
-----------------------------------------------------------------
-instance Pretty Expr where
-  ppr p e =
-    case e of
-      Lit (LInt a) -> text (show a)
-      Lit (LBool b) -> text (show b)
-      Var x -> text x
-      App a b -> parensIf (p > 0) $ (ppr (p + 1) a) <+> (ppr p b)
-      Lam x a ->
-        parensIf (p > 0) $
-        char '\\' <> hsep (flmap pp (viewVars e)) <+>
-        "->" <+> ppr (p + 1) (viewBody e)
-
+-----------------------------------------------------------------------------------
 ppexpr :: Expr -> String
 ppexpr = render . ppr 0
-----------------------------------------------------------------
+-----------------------------------------------------------------------------------
