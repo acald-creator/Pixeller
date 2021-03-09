e38602d2a1a822589dffabfca36d7b5afa1fd547
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-08 21:53:39 -0600

    Added Pretty module

--- tutorial/02_Chapter-3/ch3calcapp/app/Main.hs
+++ tutorial/02_Chapter-3/ch3calcapp/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Eval
 import Parser
+import Pretty
 
 import Control.Monad.Trans
 import System.Console.Haskeline
@@ -12,7 +13,10 @@ process line = do
   let res = parseExpr line
   case res of
     Left err -> print err
-    Right ex -> print $ runEval ex
+    Right ex ->
+      case eval ex of
+        Nothing -> putStrLn "Cannot evaluate"
+        Just result -> putStrLn $ ppexpr result
 
 main :: IO ()
 main = runInputT defaultSettings loop
