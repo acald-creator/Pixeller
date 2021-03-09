1239d70d410d8a79caeb6cf10a2e9d53b2c25465
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-08 21:03:03 -0600

    Added lexer, prefix operators, if/then/else, constants

--- tutorial/02_Chapter-3/ch3calcapp/app/Parser.hs
+++ tutorial/02_Chapter-3/ch3calcapp/app/Parser.hs
@@ -4,6 +4,7 @@ import Text.Parsec
 
 import qualified Text.Parsec.Token as Tok
 
+---------------------------------------------------------------------------------
 langDef :: Tok.LanguageDef ()
 langDef =
   Tok.LanguageDef
@@ -19,3 +20,66 @@ langDef =
     , Tok.reservedOpNames = reservedOps
     , Tok.caseSensitive = True
     }
+
+---------------------------------------------------------------------------------
+--- Lexer
+---------------------------------------------------------------------------------
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser langDef
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+reserved :: String -> Parser ()
+reserved = Tok.reserved lexer
+
+semiSep :: Parser a -> Parser [a]
+semiSep = Tok.semiSep lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Tok.reservedOp lexer
+
+prefixOp :: String -> (a -> a) -> Ex.Operator String () Identity a
+prefixOp s f = Ex.Prefix (reservedOp s >> return f)
+
+--------------------------------------------------------------------------------
+--- Prefix operators
+--------------------------------------------------------------------------------
+table :: Ex.OperatorTable String () Identity Expr
+table = [[prefixOp "succ" Succ, prefixOp "pred" Pred, prefixOp "iszero" IsZero]]
+
+--------------------------------------------------------------------------------
+--- if/then/else
+--------------------------------------------------------------------------------
+ifthen :: Parser Expr
+ifthen = do
+  reserved "if"
+  cond <- expr
+  reservedOp "then"
+  tr <- expr
+  reserved "else"
+  fl <- expr
+  return (If cond tr fl)
+
+------------------------------------------------------------------------------
+--- Constants
+------------------------------------------------------------------------------
+true, false, zero :: Parser Expr
+true = reserved "true" >> return Tr
+
+false = reserved "false" >> return Fl
+
+zero = reservedOp "0" >> return Zero
+
+expr :: Parser Expr
+expr = Ex.buildExpressionParser table factor
+
+factor :: Parser Expr
+factor = true <|> false <|> zero <|> ifthen <|> parens expr
+
+contents :: Parser a -> Parser a
+contents p = do
+  Tok.whiteSpace lexer
+  r <- p
+  eof
+  return r
