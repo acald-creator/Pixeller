e83aa1a2928516bbd8e15bc63fdf31bc210939de
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 15:16:16 -0600

    Setup the imports

--- tutorial/04_Chapter-5/c5calctyped/app/Parser.hs
+++ tutorial/04_Chapter-5/c5calctyped/app/Parser.hs
@@ -1,3 +1,14 @@
 module Parser where
 
 ---------------------------------------------------------
+import Syntax
+
+import Text.Parsec
+import Text.Parsec.Language
+import Text.Parsec.String
+
+import qualified Text.Parsec.Expr as Ex
+import qualified Text.Parsec.Token as Tok
+
+import Data.Functor.Identity
+---------------------------------------------------------
