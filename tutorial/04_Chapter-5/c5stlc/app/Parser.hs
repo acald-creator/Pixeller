d88f804065c2b3f2e5ade8fd67340651b4fdab03
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 16:30:34 -0600

    Broke up the parser in two different parts and moved the lexer section to its own file

--- tutorial/04_Chapter-5/c5stlc/app/Parser.hs
+++ tutorial/04_Chapter-5/c5stlc/app/Parser.hs
@@ -1,1 +1,67 @@
 module Parser where
+
+import Lexer
+import Syntax
+
+import Text.Parsec
+import Text.Parsec.String
+
+import qualified Text.Parsec.Expr as Ex
+
+-------------------------------------------------------------------------------
+-- Expression
+-------------------------------------------------------------------------------
+variable :: Parser Expr
+variable = do
+  x <- identifier
+  return (Var x)
+
+number :: Parser Expr
+number = do
+  n <- natural
+  return (Lit (LInt (fromIntegral n)))
+
+lambda :: Parser Expr
+lambda = do
+  reservedOp "\\"
+  x <- identifier
+  reservedOp ":"
+  t <- type'
+  reservedOp "."
+  e <- expr
+  return (Lam x t e)
+
+bool :: Parser Expr
+bool =
+  (reserved "True" >> return (Lit (LBool True))) <|>
+  (reserved "False" >> return (Lit (LBool False)))
+
+term :: Parser Expr
+term = parens expr <|> bool <|> number <|> variable <|> lambda
+
+expr :: Parser Expr
+expr = do
+  es <- many1 term
+  return (foldl1 App es)
+
+-------------------------------------------------------------------------------
+-- Types
+-------------------------------------------------------------------------------
+tyatom :: Parser Type
+tyatom = tylit <|> (parens type')
+
+tylit :: Parser Type
+tylit =
+  (reservedOp "Bool" >> return TBool) <|> (reservedOp "Int" >> return TInt)
+
+type' :: Parser Type
+type' = Ex.buildExpressionParser tyops tyatom
+  where
+    infixOp x f = Ex.Infix (reservedOp x >> return f)
+    tyops = [[infixOp "->" TArr Ex.AssocRight]]
+
+-------------------------------------------------------------------------------
+-- Toplevel
+-------------------------------------------------------------------------------
+parseExpr :: String -> Either ParseError Expr
+parseExpr input = parse (contents expr) "<stdin>" input
