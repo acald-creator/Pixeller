ad6ba1f95fa849fb516dc7bad091ef3987be3d80
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-03-09 10:57:15 -0600

    Created first part of lexer

--- tutorial/03_Chapter-4/ch4untype/app/Parser.hs
+++ tutorial/03_Chapter-4/ch4untype/app/Parser.hs
@@ -16,3 +16,24 @@ lexer = Tok.makeTokenParser style
 	style = haskellStyle {Tok.reservedOpNames = ops,
                               Tok.reservedNames = names,
                               Tok.commentLine = "#"}
+---------------------------------------------------------------------------------
+--- Lexer
+---------------------------------------------------------------------------------
+reserved :: String -> Parser ()
+reserved = Tok.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Tok.reservedOp lexer
+
+identifier :: Parser String
+identifier = Tok.identifier lexer
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+contents :: Parser a -> Parser a
+contents p = do
+	Tok.whiteSpace lexer
+	r <- p
+	eof
+	return r
